b7bd14442f9c15cfddc9304a4e0fa466a4fcc714
faidz <faidzagustiawan@gmail.com>
2026-06-18 06:14:50 +0700

    fix(images): add logo.clearbit.com to allowed remote patterns

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "api.dicebear.com",
       },
+      {
+        protocol: "https",
+        hostname: "logo.clearbit.com",
+      },
     ],
   },
   async rewrites() {
