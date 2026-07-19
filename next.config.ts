30fc73276a97f3b6ec7ee5466df5b127030ced75
faidz <faidzagustiawan@gmail.com>
2026-07-19 10:47:14 +0700

    chore(config): allow wikimedia images for next/image

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.pravatar.cc",
       },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
     ],
   },
   async rewrites() {
